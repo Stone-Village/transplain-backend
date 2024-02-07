d3be24d24843075e688f4b85ca3a21eed197a694
Charlie Arriaga <arriayaa@gmail.com>
2024-02-07 15:54:06 -0600

    added handlers

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
+const { createArticle, readArticle, deleteArticle, updateArticle } = require('./handlers');
 
 const PORT = process.env.PORT;
 const app = express();
@@ -15,5 +16,9 @@ database.on('error', console.error.bind(console, 'connection error:'));
 database.once('open', () => console.log+('connected'));
 
 //routes
+app.get('/test.articles', readArticle);
+app.post('/test.articles', createArticle);
+app.delete('/test.articles', deleteArticle);
+app.put('/test.articles', updateArticle);
 
 app.listen(PORT, () => console.log('listening on port', PORT));
