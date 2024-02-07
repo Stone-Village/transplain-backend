e6ae0a42f4575b920321bf9d057a7443bc743ae4
Charlie Arriaga <arriayaa@gmail.com>
2024-02-07 15:16:03 -0600

    added seed

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGODB_URI);
+const database = mongoose.connection;
+database.on('error', console.error.bind(console, 'connection error:'));
+database.once('open', () => console.log+('connected'));
 
+//routes
 
 app.listen(PORT, () => console.log('listening on port', PORT));
